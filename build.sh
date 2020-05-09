f0ea919aba569dc46a9bc13f3fcb9dd5cf6ed5a7
joelruiz2 <joelruiz2@gmail.com>
2020-05-08 23:01:38 -0500

    using non minified

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ rm -rf dist/ peerjs/
 git clone https://github.com/peers/peerjs peerjs
 cd peerjs
 git apply ../decoupling.diff
-../node_modules/.bin/parcel build --no-source-maps lib/exports.ts -d ../dist --out-file peerjs.min.js
+../node_modules/.bin/parcel build lib/exports.ts --out-file peerjs.js --no-minify
 cd ../
-cat imports.js dist/peerjs.min.js > dist/react-native-peerjs.js
-rm dist/peerjs.min.js
+cat imports.js dist/peerjs.js > dist/react-native-peerjs.js
+rm dist/peerjs.js
 
 echo "Done. dist/react-native-peerjs.js"
 
